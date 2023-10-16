672688730315c09ea7d6cf417d6156f446d10411
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-10-16 22:19:01 +0100

    change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,58 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
-
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox.
-// Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. 
-// Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента < img > в модальном окне перед открытием. 
-// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
 
 const galleryContainer = document.querySelector('.gallery');
 
-galleryItems.forEach((galleryItem) => {
-    galleryContainer.insertAdjacentHTML(
-        "beforeend",
-        `<div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
-    <img
-        class="gallery__image"
-        src=${galleryItem.preview}
-        data-source=${galleryItem.original}
-        alt=${galleryItem.description}
+const createGalleryMarkup = (items) => {
+  return items.map(
+    item => 
+    `<li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src=${item.preview}
+          data-source=${item.original}
+          alt=${item.description}
         />
-    </a>
-    </div>`
-    );
-});
+      </a>
+    </li>
+    `
+  ).join("")
+}
 
-galleryContainer.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
+galleryContainer.addEventListener('click', onClickHandler);
+window.addEventListener('keydown', onModelOpen)
+
+function onClickHandler (evt) {
+  evt.preventDefault();
+  if(evt.target.nodeName !== "IMG" ){
     return;
   }
-  
-  const example = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-  example.show();
-  window.addEventListener("keydown", onModalOpen);
+};
 
-  function onModalOpen(el) {
-    console.log(el);
-    if (el.code !== "Escape") {
-      return;
-    }
+// const instance = basicLightbox.create(`
+//   <div class="modal">
+//     <img src="${evt.target.dataset.source}" width="100vw" heigh="100vh"/>
+//   </div>
+// `)
+// instance.show();
 
-    example.close();
-    window.removeEventListener("keydown", onModalOpen);
+function onModelOpen (evt) {
+  if (evt.code !== "Escape" ) {
+    return;
   }
-});
+  console.log(evt.code);
 
- console.log(galleryItems);
+  instance.close();
+  window.removeEventListener("keydown", onModelOpen);
+}
+
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,26 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-const galleryContainer = document.querySelector(".gallery");
-
-function creatGalleryItems(items) {
-  return items.map(({ preview, original, description }) => {
-    return `<a class="gallery__item" href=${original}>
-      <img 
-        class="gallery__image" 
-          src=${preview} 
-          alt=${description}
-        </a>`}).join('');
-}
-const galleryItem = creatGalleryItems(galleryItems);
-
-galleryContainer.insertAdjacentHTML('beforeend', galleryItem);
-
-galleryContainer.addEventListener('click', onGalleryItemClick);
-
-function onGalleryItemClick(event) {
-    event.preventDefault();
-  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-};
 
 console.log(galleryItems);
+
