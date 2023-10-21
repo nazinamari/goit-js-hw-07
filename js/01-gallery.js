be56546b769507ca4f5f87e347d64e928d174e6c
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-10-21 14:49:52 +0100

    refactor

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,50 +1,98 @@
 import { galleryItems } from './gallery-items.js';
 
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 
-const createGalleryMarkup = (items) => {
-  return items.map(
-    item => 
-    `<li class="gallery__item">
-      <a class="gallery__link" href="large-image.jpg">
+function createGalleryMarkup(items) {
+  return items.map((item) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
         <img
           class="gallery__image"
-          src=${item.preview}
-          data-source=${item.original}
-          alt=${item.description}
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
         />
       </a>
     </li>
-    `
-  ).join("")
+  `).join("");
 }
 
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
 galleryContainer.addEventListener('click', onClickHandler);
-window.addEventListener('keydown', onModelOpen)
 
-function onClickHandler (evt) {
+function onClickHandler(evt) {
   evt.preventDefault();
-  if(evt.target.nodeName !== "IMG" ){
+  if (evt.target.nodeName !== "IMG") {
     return;
   }
-};
 
-// const instance = basicLightbox.create(`
+  const source = evt.target.dataset.source;
+  const modal = createModal(source);
+  modal.show();
+
+  galleryContainer.addEventListener('keydown', handleKeyDown);
+
+  function handleKeyDown(evt) {
+    if (evt.code === "Escape") {
+      modal.close();
+      galleryContainer.removeEventListener('keydown', handleKeyDown);
+    }
+  }
+}
+
+function createModal(source) {
+  const modal = basicLightbox.create(`
+    <div class="modal">
+      <img src="${source}" width="100%" height="100%"/>
+    </div>
+  `);
+  return modal;
+}
+
+// import { galleryItems } from './gallery-items.js';
+
+// const galleryContainer = document.querySelector(".gallery");
+
+// function createGalleryMarkup(items) {
+//   return items
+//   .map(
+//     (item) => 
+//     `<li class="gallery__item">
+//       <a class="gallery__link" href="large-image.jpg">
+//         <img
+//           class="gallery__image"
+//           src=${item.preview}
+//           data-source=${item.original}
+//           alt=${item.description}
+//         />
+//       </a>
+//     </li>
+//     `
+//   ).join("");
+// }
+
+// const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+// galleryContainer.innerHTML = addGalleryMarkup;
+// galleryContainer.addEventListener('click', onClickHandler);
+
+// function onClickHandler (evt) {
+//   evt.preventDefault();
+//   if(evt.target.nodeName !== "IMG" ){
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(`
 //   <div class="modal">
-//     <img src="${evt.target.dataset.source}" width="100vw" heigh="100vh"/>
+//     <img src="${evt.target.dataset.source}" width="100%" heigh="100%"/>
 //   </div>
 // `)
 // instance.show();
 
-function onModelOpen (evt) {
-  if (evt.code !== "Escape" ) {
-    return;
-  }
-  console.log(evt.code);
-
-  instance.close();
-  window.removeEventListener("keydown", onModelOpen);
-}
-
-
+// galleryContainer.addEventListener('keydown', evt => {
+//   if (evt.code === "Escape") {
+//     instance.close();
+//   }
+// });
+// };
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
 
+function createGalleryMarkup(items) {
+    return items.map((item) => `
+        <li class="">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li>
+    `).join('');
+}
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+const options = {
+captionsData: "alt",
+captionPosition: "bottom",
+captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox(".gallery a", options);
